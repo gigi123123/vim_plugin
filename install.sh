5fc10c7a84dda26e0bda6d072f39e0904f86ad69
YouliangZhang <1577121881@qq.com>
2018-08-05 12:37:43 +0800

    init

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#git clone https://gitee.com/whatdy/vim_plugin.git
+git clone https://gitee.com/whatdy/vim_plugin.git
 # rm -rf .vim/bundle/*/.git
 cp -i vim_plugin/.vimrc $HOME/.vimrc
 cp -i vim_plugin/.vimrc.bundles $HOME/.vimrc.bundles
