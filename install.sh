1236d2dc60fb1dc66c6bb8e5e27051d440abb2c8
YouliangZhang <1577121881@qq.com>
2018-08-05 17:08:02 +0800

    每日关键词统计

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 git clone https://gitee.com/whatdy/vim_plugin.git
 # rm -rf .vim/bundle/*/.git
-cp -i vim_plugin/.vimrc $HOME/.vimrc
-cp -i vim_plugin/.vimrc.bundles $HOME/.vimrc.bundles
-cp -ri vim_plugin/.vim $HOME/.vim
+mkdir vim_plugin/back
+
+# 备份原有 vim 数据
+mv .vim* vim_plugin/back
+cp vim_plugin/.vimrc $HOME/.vimrc
+cp vim_plugin/.vimrc.bundles $HOME/.vimrc.bundles
+cp -rf vim_plugin/.vim $HOME/.vim
 
