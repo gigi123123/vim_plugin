ede974e5fe148cc534ad0c4bc77f4289a3154182
YouliangZhang <1577121881@qq.com>
2018-08-19 18:19:59 +0800

    警告去除

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+
 rm -rf ~/.vimrc ~/.vimrc.bundles ~/.vim
 
-
-mv -f ~/.back/.vimrc ~/.vimrc
-mv -f ~/.back/.vimrc.bundles ~/.vimrc.bundles
-mv -f ~/.back/.vim ~/.vim
+if [ -f "~/back/.vim" ];then
+    mv -f ~/back/.vim ~/.vim
+fi
+if [ -f "~/back/.vimrc" ];then
+    mv -f ~/back/.vimrc ~/.vimrc
+fi
+if [ -f "~/back/.vimrc.bundles" ];then
+    mv -f ~/back/.vimrc.bundles ~/.vimrc.bundles
+fi
 
 rm -rf ~/.back
